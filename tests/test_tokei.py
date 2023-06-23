fd8f92968470f6d26697acd88004604fd1132b70
Grant Jenks <grant.jenks@gmail.com>
2023-06-22 23:50:47 -0700

    Tests for path and bad path

--- tests/test_tokei.py
+++ tests/test_tokei.py
@@ -1,8 +1,17 @@
-"""Test Python wrapper for Tokei
-"""
-
+"""Test Python wrapper for Tokei"""
+import pytest
 import tokei
 
 
 def test_title():
     assert tokei.__title__ == 'tokei'
+
+
+def test_tokei():
+    result = tokei.tokei(__file__)
+    assert result['Total']['code'] > 0
+
+
+def test_tokei_bad_path():
+    with pytest.raises(Exception):
+        tokei.tokei('does-not-exist')
